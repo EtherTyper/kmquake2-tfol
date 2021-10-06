91d3869249ff54907e0f27881e1954cf73e515b6
Ethan Lee <flibitijibibo@gmail.com>
2021-10-05 22:26:08 -0400

    Window starts up, sloppy but it exists

--- unix/gl_sdl.c
+++ unix/gl_sdl.c
@@ -21,14 +21,25 @@ void GLimp_Shutdown(void)
 
 void GLimp_BeginFrame(float camera_seperation)
 {
+	SDL_ShowWindow(glw_state.glWindow);
 }
 
 void GLimp_EndFrame(void)
 {
+	SDL_GL_SwapWindow(glw_state.glWindow);
 }
 
 int GLimp_SetMode(int *pwidth, int *pheight, int mode, dispType_t fullscreen)
 {
+	int width, height;
+	if (!VID_GetModeInfo(&width, &height, mode))
+	{
+		Com_Printf(" invalid mode\n");
+		return rserr_invalid_mode;
+	}
+	*pwidth = width;
+	*pheight = height;
+	VID_NewWindow(width, height);
 	return rserr_ok;
 }
 
@@ -47,4 +58,12 @@ void IN_Activate(qboolean active)
 
 void HandleEvents(void)
 {
+	SDL_Event evt;
+	while (SDL_PollEvent(&evt) == 1)
+	{
+		if (evt.type == SDL_QUIT)
+		{
+			Cbuf_ExecuteText(EXEC_NOW, "quit");
+		}
+	}
 }
